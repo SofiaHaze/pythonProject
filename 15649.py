09f07ffb5c5fc6b6ea63425bcbadf95c8b5279e0
SofiaHaze <jsm9891@gmail.com>
2023-01-19 09:56:56 +0900

    14425 strings

--- 15649.py
+++ 15649.py
@@ -1,39 +1,19 @@
-n, m = map(int, input().split())
-s = []
-visited = [False] * (n+1)
+import sys
 
-#재귀함수를 이용하여 m개의 수열을 저장하기 위한 리스트
-s = []
+n, m = map(int, sys.stdin.readline().split())
 
-def dfs():
-    #(함수 출력조건) 리스트 s안에 m개의 요소가 쌓인다면 출력해주도록 한다.
-    if len(s) == m:
-        print(s)
-        #[1, 2, 3] > 1 2 3
-        print("m==s ok")
-        print(' '.join(map(str, s)))
-        return #함수를 빠져나가는 방법
+s = [str(sys.stdin.readline()) for _ in range(n)] # 집합 s
+cnt = 0
 
-    for i in range(1, n + 1):
-        print(f'i실행합니다용{i}')
-        if visited[i]:
-            continue # True이면, 아래 코드를 실행하지 않고 건너뜀
-        visited[i] = True
-        s.append(i)
-        print(f'dfs실행 직전:{s}')
+# 반복문을 통해 m개의 문자열을 확인
+for i in range(m):
+    word = str(sys.stdin.readline())
 
-        dfs()
-        #i=1부터 반복되는데 visited[1] = True이기 때문에 조건문에 걸려 continue 된다 s=[1]인상태 , i=2 때 s=[1,2]
-        #재귀 함수 만약 n=4, m=2라면 밑과 같은 형태로 진행될 것이다.      s : [1] -> [1,2] -> [1] -> [1,3] -> [1] -> [1,4]
-        print("i : %d " % (i))
-        print(f'dfs실행 직후:{s}')
-        print(visited)
-        s.pop() #리스트의 맨 마지막 원소를 리턴하고 해당 원소는 삭제
-        print(f'backTracking:{s}')
-        visited[i] = False
-        print(visited)
+    # 집합 s에 문자열이 포함되어 있으면 카운트
+    if word in s:
+        cnt += 1
 
-dfs()
+print(cnt)
 
 
 
