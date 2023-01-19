5349a9bbe95f8f4099b49e0659c61f9f0e66cef2
SofiaHaze <jsm9891@gmail.com>
2023-01-19 10:23:48 +0900

    백준_14425_문자열 집합

---
+++ 14425.py
@@ -0,0 +1,16 @@
+import heapq
+#데이터 정렬 된 상태로 저장하기 위해서 사용하는 파이썬 내장모듈
+
+heap = []
+n = int(input())
+
+for _ in range(n):
+    numbers = list(map(int, input().split())) #map 반환값 map 객체  list or tuple
+    for number in numbers:
+        if len(heap) < n: # heap의 크기를 n개로 유지
+            heapq. heappush(heap, number)
+        else:
+            if heap[0] < number:
+                heapq.heappop(heap)
+                heapq.heappush(heap, number)
+print(heap[0])
